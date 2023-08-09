40945a3889972eadf4f0ede54c7744bb20438145
Rafael Santos <rafa.05125@gmail.com>
2023-08-09 09:30:54 -0300

    Find Product by id test

---
+++ dscommerce-restassured/dscommerce-restassured/src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
@@ -0,0 +1,35 @@
+package com.devsuperior.dscommerce.controllers;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.is;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ProductControllerRA {
+
+	private Long existingProductId, nonExistingProductId;
+
+	@BeforeEach
+	public void setUp() {
+		baseURI = "http://localhost:8080";
+	}
+
+	@Test
+	public void findByIdShouldReturnProductWhenIdExists() {
+		existingProductId = 2L;
+
+		given().get("/products/{id}", 2L)
+				.then().statusCode(200)
+				.body("id", is(2))
+				.body("name", equalTo("Smart TV"))
+				.body("imgUrl", equalTo(
+						"https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"))
+				.body("price", is(2190.0F))
+				.body("categories.id", hasItems(2,3))
+				.body("categories.name", hasItems("Eletrônicos","Computadores"));
+	}
+}
